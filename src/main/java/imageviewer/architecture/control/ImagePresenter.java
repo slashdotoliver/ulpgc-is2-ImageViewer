1e9afb6008caa1a98f7154f519e4152914eb7404
Oliver Cabrera <oliver.cabrera101@alu.ulpgc.es>
2024-12-12 14:51:05 +0000

    Add ImagePresenter

---
+++ src/main/java/imageviewer/architecture/control/ImagePresenter.java
@@ -0,0 +1,23 @@
+package imageviewer.architecture.control;
+
+import imageviewer.architecture.control.io.ImageLoader;
+import imageviewer.architecture.model.Image;
+import imageviewer.architecture.view.ImageDisplay;
+
+public class ImagePresenter {
+
+    private final ImageDisplay imageDisplay;
+
+    private Image currentImage;
+
+    public ImagePresenter(ImageDisplay imageDisplay, ImageLoader loader) {
+        this.imageDisplay = imageDisplay;
+        imageDisplay.setPreviousButtonListener(() -> show(currentImage.previous()));
+        imageDisplay.setNextButtonListener(() -> show(currentImage.next()));
+        show(loader.load());
+    }
+
+    private void show(Image image) {
+        imageDisplay.show(this.currentImage = image);
+    }
+}
