f5815fcfc01f1b284f719bc3e0c74a75849f4e91
Oliver Cabrera <oliver.cabrera101@alu.ulpgc.es>
2024-12-14 19:32:29 +0000

    Add SimpleFolderDialog

---
+++ src/main/java/imageviewer/apps/swingsimple/view/SimpleFolderDialog.java
@@ -0,0 +1,21 @@
+package imageviewer.apps.swingsimple.view;
+
+import imageviewer.architecture.view.FolderDialog;
+
+import javax.swing.*;
+import java.io.File;
+import java.util.Optional;
+
+public class SimpleFolderDialog implements FolderDialog {
+    @Override
+    public Optional<File> get() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        int option = fileChooser.showOpenDialog(new JFrame("Select Folder"));
+
+        if (option == JFileChooser.APPROVE_OPTION)
+            return Optional.of(fileChooser.getSelectedFile());
+        return Optional.empty();
+    }
+}
