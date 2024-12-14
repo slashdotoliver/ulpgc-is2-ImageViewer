0c397b9490dfe12686bbca7661fd13c163614344
Oliver Cabrera <oliver.cabrera101@alu.ulpgc.es>
2024-12-14 19:27:41 +0000

    Add Image.None

--- src/main/java/imageviewer/architecture/model/Image.java
+++ src/main/java/imageviewer/architecture/model/Image.java
@@ -1,10 +1,42 @@
 package imageviewer.architecture.model;
 
 import java.io.IOException;
+import java.util.StringJoiner;
 
 public interface Image {
     byte[] content() throws IOException;
     Image next();
     Image previous();
     String name();
+
+    Image None = new Image() {
+        @Override
+        public byte[] content() {
+            return null;
+        }
+
+        @Override
+        public Image next() {
+            return None;
+        }
+
+        @Override
+        public Image previous() {
+            return None;
+        }
+
+        @Override
+        public String name() {
+            return "";
+        }
+
+        @Override
+        public String toString() {
+            return new StringJoiner(", ", Image.class.getSimpleName() + ".None[", "]")
+                    .add("name: '" + name() + "'")
+                    .add("previous: '" + previous().name() + "'")
+                    .add("next: '" + next().name() + "'")
+                    .toString();
+        }
+    };
 }
