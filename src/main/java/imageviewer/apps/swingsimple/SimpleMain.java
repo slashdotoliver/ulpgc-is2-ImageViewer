d253ec62613bfc43b99beb14454bacef60db8829
Oliver Cabrera <oliver.cabrera101@alu.ulpgc.es>
2024-12-14 19:37:29 +0000

    Refactor SimpleMain: registerOpenFolderCommand() and handleArguments()

--- src/main/java/imageviewer/apps/swingsimple/SimpleMain.java
+++ src/main/java/imageviewer/apps/swingsimple/SimpleMain.java
@@ -1,34 +1,64 @@
 package imageviewer.apps.swingsimple;
 
+import imageviewer.apps.swingsimple.control.ProgramArguments;
+import imageviewer.apps.swingsimple.view.SimpleFolderDialog;
 import imageviewer.apps.swingsimple.view.SimpleMainFrame;
 import imageviewer.architecture.control.ImagePresenter;
+import imageviewer.architecture.control.commands.Command;
+import imageviewer.architecture.control.commands.CommandName;
+import imageviewer.architecture.control.commands.OpenImageFolderCommand;
 import imageviewer.architecture.control.io.FolderImageLoader;
+import imageviewer.architecture.view.ErrorDisplay;
 
 import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 import java.util.logging.Logger;
 
 public class SimpleMain {
 
+    private static final Map<CommandName, Command> COMMANDS = new HashMap<>();
     private static final Logger LOGGER = Logger.getLogger(SimpleMain.class.getSimpleName());
     private static ImagePresenter presenter;
+    private static SimpleMainFrame mainFrame;
 
     public static void main(String[] args) {
         changeLookAndFeel();
-        var mainFrame = new SimpleMainFrame();
-        try {
-            presenter = new ImagePresenter(
-                    mainFrame.getImageDisplay(),
-                    new FolderImageLoader(new File("./images"))
-            );
-        } catch (IOException | FolderImageLoader.EmptyImageFolderException e) {
-            LOGGER.severe(e.getMessage());
-        }
+
+        mainFrame = new SimpleMainFrame();
+        presenter = new ImagePresenter(mainFrame.getImageDisplay());
+
+        handleArguments(args);
+        registerOpenFolderCommand();
         mainFrame.setVisible(true);
+    }
+
+    private static void handleArguments(String[] args) {
+        ProgramArguments arguments = new ProgramArguments(args);
+        if (!arguments.valid())
+            System.err.println(arguments.validFormat());
+        else if (arguments.hasFolder())
+            tryShowing(arguments.folder(), mainFrame.getErrorDisplay());
+    }
+
+    private static void registerOpenFolderCommand() {
+        COMMANDS.put(CommandName.OpenFolderCommand, new OpenImageFolderCommand(
+                mainFrame.getErrorDisplay(),
+                new SimpleFolderDialog(),
+                presenter
+        ));
+        mainFrame.getOpenFolderMenuItem().addActionListener(
+                _ -> COMMANDS.get(CommandName.OpenFolderCommand).execute()
+        );
+    }
+
+    private static void tryShowing(File folder, ErrorDisplay errorDisplay) {
+        try {
+            presenter.loadUsing(new FolderImageLoader(folder));
+        } catch (IOException | FolderImageLoader.EmptyImageFolderException e) {
+            errorDisplay.show(e.getMessage());
+        }
     }
 
     private static void changeLookAndFeel() {
